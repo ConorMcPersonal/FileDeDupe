7f8d7645024b931454d36cef370b20caa79c0988
Conor <conor.mcmenamin@gmail.com>
2024-12-08 15:03:13 +0000

    Small improvement using scandir

--- walker.py
+++ walker.py
@@ -13,12 +13,18 @@ class Walker:
             folder_path = self.path
 
         # traverse root directory, and list directories as dirs and files as files
-        for root, dirs, files in os.walk(folder_path):
-            print (list(os.scandir(root)))
-            path = root.split(os.sep)
-            print((len(path) - 1) * '---', os.path.basename(root))
-            for file in files:
-                print(len(path) * '---', file)
+        subdirs_to_check = []
+        for direntry in os.scandir(folder_path):
+            if direntry.is_dir():
+                subdirs_to_check.append(direntry)
+            else:
+                path = direntry.path.split(os.sep)
+                print((len(path) - 1) * '---', os.path.basename(direntry.path), direntry.stat())
+
+        for direntry in subdirs_to_check:
+            print()
+            print (direntry.path)
+            self.walk_tree(folder_path = direntry.path)
 
 w = Walker("..")
 w.walk_tree()
